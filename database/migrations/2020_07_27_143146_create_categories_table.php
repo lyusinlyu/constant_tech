de2e18cf0421c68359389d58b2aaae78788a8a99
lus <lusinehovh@gmail.com>
2020-07-27 20:25:08 +0400

    First commit

---
+++ database/migrations/2020_07_27_143146_create_categories_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateCategoriesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('categories', function (Blueprint $table) {
+            $table->id();
+            $table->string('name');
+            $table->integer('order');
+            $table->bigInteger('parent_id')->unsigned()->nullable();
+            $table->timestamps();
+            $table->foreign('parent_id')->references('id')->on('categories');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('categories', function (Blueprint $table) {
+            $table->dropForeign('categories_parent_id_foreign');
+        });
+        Schema::dropIfExists('categories');
+    }
+}
